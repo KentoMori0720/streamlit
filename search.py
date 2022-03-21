4fb2bfe15f40a95004dcbd30b04072ffbbc6ad78
KentoMori0720 <kento.0720.pri@gmail.com>
2022-03-22 06:33:46 +0900

    コミット

--- search.py
+++ search.py
@@ -1,17 +1,10 @@
-from turtle import up
 from azure.cognitiveservices.vision.computervision import ComputerVisionClient
 from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
 from azure.cognitiveservices.vision.computervision.models import VisualFeatureTypes
 from msrest.authentication import CognitiveServicesCredentials
 
-from array import array
-import os
 from PIL import Image
-import sys
-import time
 from numpy import imag
-
-import streamlit
 
 KEY = "40dfae1c43cc4e0ab00926705f64891f"
 ENDPOINT = "https://20220316test.cognitiveservices.azure.com/"
@@ -36,6 +29,8 @@ def detect_objects(filepath):
     return objects
 
 import streamlit as st
+from PIL import ImageDraw
+from PIL import ImageFont
 
 st.title('物体検出アプリ')
 
@@ -44,8 +39,28 @@ if uploaded_file is not None:
     img = Image.open(uploaded_file)
     img_path = f'img/{uploaded_file.name}'
     img.save(img_path)
+    objects = detect_objects(img_path)
+
+    # 描画
+    draw = ImageDraw.Draw(img)
+    for object in objects:
+        x = object.rectangle.x
+        y = object.rectangle.y
+        w = object.rectangle.w
+        h = object.rectangle.h
+        caption = object.object_property
+
+        font = ImageFont.truetype(font='./Helvetica 400.ttf', size=50)
+        text_w, text_h = draw.textsize(caption, font=font)
+        
+        draw.rectangle([(x, y),(x+w, y+h)], fill=None, outline='green', width=5 )
+        draw.rectangle([(x, y),(x+text_w, y+text_h)], fill='green', width=5 )
+        draw.text((x, y), caption, fill='white', font=font)
+
     st.image(img)
 
+    tags_name = get_tags(img_path)
+    tags_name = ', '.join(tags_name)
 
     st.markdown('**認識されたコンテンツタグ**')
-    st.markdown('> apple, tree, building, green')
+    st.markdown(f'> {tags_name}')
